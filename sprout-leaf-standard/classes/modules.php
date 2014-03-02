c0fb127f42e022e06abed62749df55111721e695
Justin Proffitt <justin@bnpdesigngroup.com>
2014-03-01 22:21:01 -0700

    Reloading with subtrees

--- sprout-leaf-standard/classes/modules.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-// Load modules
-
-require dirname(__FILE__) . '/modules/class-leaf-options.php';
-require dirname(__FILE__) . '/modules/class-leaf-scripts.php';
-require dirname(__FILE__) . '/modules/class-leaf-sidebars.php';
-
-?>
